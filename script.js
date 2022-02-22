9813f2b07423a24b1120f7656ad83fcd1483e2bc
Vannent <98601749+Vannent@users.noreply.github.com>
2022-02-22 11:25:19 -0600

    Loop created.

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-let PlayerSelection = prompt("Rock, Paper or Scissors?").toLowerCase;
-
-function computerPlay() {
+for (let i = 0; i < 5; i++) {
+    function computerPlay() {
     let computerOptions = ["rock", "paper", "scissors"];
     let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    console.log('Computer selected:', computerChoice)
+    console.log('Computer selected: ' + computerChoice)
     return computerChoice
-}
+    }
+    let PlayerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
 
-for (let i = 0; i < 5; i++) {
-    PlayerSelection;
-    computerPlay();
-}
+    if (PlayerSelection!= undefined && PlayerSelection === "paper", "scissors", "rock") {
+        if (PlayerSelection === "rock" && computerPlay() === "scissors") {
+        alert("Congratulations, you've won!");
+        console.log('Player selected: ', PlayerSelection);
+        } else if (PlayerSelection === "scissors" && computerPlay() === "paper") {
+        alert("Congratulations, you've won!");
+        console.log('Player selected: ', PlayerSelection);
+        } else if (PlayerSelection === "paper" && computerPlay() === "rock") {
+        alert("Congratulations, you've won!");
+        console.log('Player selected: ', PlayerSelection);
+        } else {
+        alert("Unfortunately, you've lost!");
+        console.log('Player selected: ', PlayerSelection);
+        }
+    } else {
+        alert("You need to make a correct selection.");
+    }
+}   
