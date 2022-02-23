f0a36c9c1ca788e0de311546d16bc05cab774f66
Vannent <98601749+Vannent@users.noreply.github.com>
2022-02-23 17:13:15 -0600

    Added score system

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 for (let i = 0; i < 5; i++) {
     function computerPlay() {
     let computerOptions = ["rock", "paper", "scissors"];
@@ -6,22 +9,41 @@ for (let i = 0; i < 5; i++) {
     return computerChoice
     }
     let PlayerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-
     if (PlayerSelection!= undefined && PlayerSelection === "paper", "scissors", "rock") {
         if (PlayerSelection === "rock" && computerPlay() === "scissors") {
-        alert("Congratulations, you've won!");
-        console.log('Player selected: ', PlayerSelection);
+            playerScore += 1
+            alert("You won!")
+            console.log('Player selected: ', PlayerSelection);
         } else if (PlayerSelection === "scissors" && computerPlay() === "paper") {
-        alert("Congratulations, you've won!");
-        console.log('Player selected: ', PlayerSelection);
+            playerScore += 1
+            alert("You won!")
+            console.log('Player selected: ', PlayerSelection);
         } else if (PlayerSelection === "paper" && computerPlay() === "rock") {
-        alert("Congratulations, you've won!");
-        console.log('Player selected: ', PlayerSelection);
+            playerScore += 1
+            alert("You won!")
+            console.log('Player selected: ', PlayerSelection);
+        } else if (PlayerSelection === "paper" && computerPlay() === "paper") {
+            alert("Draw!")
+            console.log('Player selected: ', PlayerSelection);
+        } else if (PlayerSelection === "rock" && computerPlay() === "rock") {
+            alert("Draw!")
+            console.log('Player selected: ', PlayerSelection);
+        } else if (PlayerSelection === "scissors" && computerPlay() === "scissors") {
+            alert("Draw!")
+            console.log('Player selected: ', PlayerSelection);
         } else {
-        alert("Unfortunately, you've lost!");
-        console.log('Player selected: ', PlayerSelection);
+            computerScore += 1
+            alert("You lost!")
+            console.log('Player selected: ', PlayerSelection);
         }
     } else {
         alert("You need to make a correct selection.");
     }
 }   
+
+if (playerScore >= 3) {
+    alert("Congratulations, you've won!")
+} else {
+    alert("Unfortunately, you've lost! Try again!")
+}
+console.log(playerScore, computerScore)
